0020e52ead9bd683c86b0d6f6af75adb03366c20
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-03-17 13:06:31 +0200

    update api

--- lib/config.ts
+++ lib/config.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = 'https://manfaz-production.up.railway.app';
+// export const BASE_URL = 'https://manfaz-production.up.railway.app';
+// export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://manfaz.fly.dev';
