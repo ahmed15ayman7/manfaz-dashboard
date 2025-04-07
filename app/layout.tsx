9282b106956d83455fba9b50cebc79374f464da2
ahmed15ayman7 <ahmed15ayman7ahmed2002@gmail.com>
2025-04-07 22:07:56 +0200

    update icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ar" dir="rtl">
+      <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      </head>
       <body className={tajawal.className}>
         <Providers>
           {children}
